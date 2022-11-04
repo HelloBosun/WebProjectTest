31ea59ee313de444ce302fba67f327c9104279b3
HelloBosun <tubosunbiz@gmail.com>
2022-11-04 18:32:29 +0100

    Add project files.

---
+++ Controllers/ContactsController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebProjectTest.Models;
+
+namespace WebProjectTest.Controllers
+{
+    [Route("api/Contacts")]
+    [ApiController]
+    public class ContactsController : Controller
+    {
+        private List<Contacts> contacts = new List<Contacts>
+        {
+            new Contacts{Id = 1, FirstName = "Developer", LastName = "Code", NickName= "Smart", Place= "Lagos", DateCreated = DateTime.Now, IsDeleted = false},
+            new Contacts{Id = 2, FirstName = "Channel", LastName= "People", NickName= "Today", Place= "Ikoyi"}
+        };
+
+        // // GET: ContactsController
+        [HttpGet]
+        public IEnumerable<Contacts> Get()
+        {
+            return contacts;
+        }
+
+        // // GET: ContactsController/Details/5
+        [HttpGet("{id}")]
+        public ActionResult<Contacts> Get(int id)
+        {
+            var contact = contacts.FirstOrDefault(c => c.Id == id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+            return Ok(contact);
+        }
+
+        // // GET: ContactsController/Create
+        // public ActionResult Create()
+        // {
+        //     return View();
+        // }
+
+        // POST: ContactsController/Create    
+        [HttpPost]
+        // [ValidateAntiForgeryToken]
+        public ActionResult<IEnumerable<Contacts>> Post(Contacts newContacts)
+        {
+            contacts.Add(newContacts);
+            return contacts;
+        }
+
+        //    // GET: ContactsController/Edit/5
+        //    public ActionResult Edit(int id)
+        //    {
+        //        return View();
+        //    }
+
+        //    // POST: ContactsController/Edit/5
+        //   // [HttpPost]
+        //    [ValidateAntiForgeryToken]
+        //    public ActionResult Edit(int id, IFormCollection collection)
+        //    {
+        //        try
+        //        {
+        //            return RedirectToAction(nameof(Index));
+        //        }
+        //        catch
+        //        {
+        //            return View();
+        //        }
+        //    }
+
+        //    // GET: ContactsController/Delete/5
+        //    public ActionResult Delete(int id)
+        //    {
+        //        return View();
+        //    }
+
+        //    // POST: ContactsController/Delete/5
+        //  //  [HttpPost]
+        //    [ValidateAntiForgeryToken]
+        //    public ActionResult Delete(int id, IFormCollection collection)
+        //    {
+        //        try
+        //        {
+        //            return RedirectToAction(nameof(Index));
+        //        }
+        //        catch
+        //        {
+        //            return View();
+        //        }
+        //    }
+        //}
+    }
+}
